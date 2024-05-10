21f110303b12749efc058573935e665d6c0b37ad
Nadir GHARBI <nadirgharbi10@gmail.com>
2024-05-10 16:20:51 +0200

    change speciality validator to optional

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -28,7 +28,7 @@ export const registerValidator = vine.compile(
       .minLength(8)
       .maxLength(32),
 
-    speciality: vine.string(),
+    speciality: vine.string().optional(),
     phone: vine.string().optional(),
     office_name: vine.string().optional(),
     adress: vine.string().optional(),
